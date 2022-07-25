f59d85b4179d5a7b1af058df188d4d1722ff0c32
Wanamaker <dla4038@naver.com>
2022-07-25 16:46:14 +0900

    day1

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const a = 10;
+const b = 2;
+const JoYull = "wanamaker";
 
-console.log(a + 2);
-console.log(a * 2);
-console.log(a / 2);
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + JoYull);
