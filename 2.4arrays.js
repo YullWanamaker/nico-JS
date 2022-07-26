d4ea1db2bf4d3075e00ab861e4222fe0a763275b
Wanamaker <dla4038@naver.com>
2022-07-26 17:50:22 +0900

    Update 2.4arrays.js

--- 2.4arrays.js
+++ 2.4arrays.js
@@ -1,11 +1,9 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fir";
-const sat = "sat";
-const sun = "sun";
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-const daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
-
+//get Item from Array
 console.log(daysOfWeek);
+
+//Add one more day to the array
+daysOfWeek.push("dya");
+
+const toBuy = ["potato", "tomato", "pizza"];
