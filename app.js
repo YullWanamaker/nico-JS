e076b2d98515bda832399fd59995b80b7164781a
Wanamaker <dla4038@naver.com>
2022-07-26 17:28:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-let a = 3;
-let b = 2;
-let JoYull = "wanamaker";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + JoYull);
-
-JoYull = "coingrandfatehr";
-console.log("your new name is " + JoYull);
+const amIFat = true;
+console.log(amIFat);
