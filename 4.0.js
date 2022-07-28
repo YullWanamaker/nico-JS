9077b4470af1cfc97782018e273107c738529dbc
Wanamaker <dla4038@naver.com>
2022-07-28 18:54:40 +0900

    4.1

--- 4.0.js
+++ 4.0.js
@@ -4,10 +4,5 @@ const loginButton = loginForm.querySelector("button");
 
 function ButtonClick() {
   const username = loginInput.value;
-  if (username === "") {
-    alert("please write your name");
-  } else if (username.length > 15) {
-    alert("your name is too long,");
-  }
 }
 loginButton.addEventListener("click", ButtonClick);
