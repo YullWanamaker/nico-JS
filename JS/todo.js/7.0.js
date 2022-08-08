ff564643ff08600cfce5f9a9f818180569f9c791
Wanamaker <dla4038@naver.com>
2022-08-08 20:53:27 +0900

    8.2

--- JS/todo.js/7.0.js
+++ JS/todo.js/7.0.js
@@ -2,12 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   console.log(toDoInput.value);
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newTodo, toDoInput.value);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
---
+++ JS/weather/8.0.js
@@ -0,0 +1,23 @@
+const API_KEY = "707496ccdcfe18b3027b9778452f402c";
+//api.openweathermap.org/data/2.5/weather?lat=36.2037&lon=127.0797&appid=707496ccdcfe18b3027b9778452f402c
+
+https: function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("you live in", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.jason())
+    .then((date) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main}/ ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
