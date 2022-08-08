f834ceeaeeecf22ae250acfc338933748dae1e42
Wanamaker <dla4038@naver.com>
2022-08-08 20:55:20 +0900

    Update 8.0.js

--- JS/weather/8.0.js
+++ JS/weather/8.0.js
@@ -1,4 +1,3 @@
-const API_KEY = "707496ccdcfe18b3027b9778452f402c";
 //api.openweathermap.org/data/2.5/weather?lat=36.2037&lon=127.0797&appid=707496ccdcfe18b3027b9778452f402c
 
 https: function onGeoOk(position) {
@@ -11,6 +10,8 @@ https: function onGeoOk(position) {
     .then((date) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
+      const API_KEY = "707496ccdcfe18b3027b9778452f402c";
+
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main}/ ${data.main.temp}`;
     });
