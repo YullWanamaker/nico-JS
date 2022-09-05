9f2afbe46dfc63a0e06836fc22a73fdf8d9f7126
Wanamaker <dla4038@naver.com>
2022-09-05 22:27:35 +0900

    1

---
+++ ver2/app2.js
@@ -0,0 +1,11 @@
+const loginForm = document.querySelector("#login-form");
+const numberInput = document.querySelector("#input");
+const button = document.querySelector("#key");
+
+function handleinputClick(event) {
+  event.preventDefault();
+  console.log(numberInput.value);
+}
+
+numberInput.addEventListener("submit", handleinputClick);
+//button.addEventListener("click", handleinputClick);
