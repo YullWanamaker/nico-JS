3e517252105a35e02d403f81be0f13466b15c93b
Wanamaker <dla4038@naver.com>
2022-09-06 19:55:00 +0900

    .

--- ver2/app2.js
+++ ver2/app2.js
@@ -1,12 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const numberInput = document.querySelector("#input");
+const firstInput = document.querySelector("#firstInput");
 const button = document.querySelector("#key");
 
 function handleinputClick(event) {
   event.preventDefault();
+  const number = Math.round(Math.randomm() * firstInput.value);
+  if (String(input.value) === String(number)) {
+    const span = document.createElement("span");
+    span.innerText = `you chose: ${input.value}, the machine chose: ${number}.`;
+
+    const h5 = document.createElement("h5");
+    h5.innerText = `you won!`;
+    span.appendChild(h5);
+    document.body.appendChild(span);
+  } else {
+    const span = document.createElement("span");
+    span.innerText = `you chose: ${input.value}, the machine chose: ${number}.`;
+    const h5 = document.createElement("h5");
+    h5.innerText = `you lost!`;
+    span.appendChild(h5);
+    document.body.appendChild(span);
+  }
+
   const inputnumber = numberInput.value;
   localStorage.setItem("inputnumber", inputnumber);
 }
 
 numberInput.addEventListener("submit", handleinputClick);
-button.addEventListener("click", handleinputClick);
