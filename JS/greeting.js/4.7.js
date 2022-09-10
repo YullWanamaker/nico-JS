57da664f3dd89d9633bbd5a8746fceb699151228
Wanamaker <dla4038@naver.com>
2022-09-10 17:43:13 +0900

    1

--- JS/greeting.js/4.7.js
+++ JS/greeting.js/4.7.js
@@ -16,6 +16,7 @@ function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+//
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
