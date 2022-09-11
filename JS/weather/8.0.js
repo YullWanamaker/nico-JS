a6e45591a2ae0c4f4dd187b748a684d22046af63
Wanamaker <dla4038@naver.com>
2022-09-11 18:58:39 +0900

    1

--- JS/weather/8.0.js
+++ JS/weather/8.0.js
@@ -1,19 +1,20 @@
 //api.openweathermap.org/data/2.5/weather?lat=36.2037&lon=127.0797&appid=707496ccdcfe18b3027b9778452f402c
+const API_KEY = "707496ccdcfe18b3027b9778452f402c";
 
-https: function onGeoOk(position) {
+function onGeoOk(position) {
+  console.log(position);
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("you live in", lat, lng);
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  const log = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
   fetch(url)
-    .then((response) => response.jason())
-    .then((date) => {
+    .then((response) => response.json())
+    .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
-      const API_KEY = "707496ccdcfe18b3027b9778452f402c";
 
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main}/ ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
